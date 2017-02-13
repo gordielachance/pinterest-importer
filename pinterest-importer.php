96804e3b77a291daa67e24ed22608b47ea0987f0
rickybiscus <gordie.lachance@gmail.com>
2017-02-13 20:43:01 +0100

    use custom post type for pins (were posts before) + upgrade routine

--- pinterest-importer.php
+++ pinterest-importer.php
@@ -16,12 +16,12 @@ class PinIm {
     /**
     * @public string plugin version
     */
-    public $version = '0.4.7';
+    public $version = '0.4.8';
 
     /**
     * @public string plugin DB version
     */
-    public $db_version = '207';
+    public $db_version = '208';
 
     /** Paths *****************************************************************/
 
@@ -43,6 +43,7 @@ class PinIm {
     public $options_default = array();
     public $options = array();
 
+    static $pin_post_type = 'pin';
     static $meta_name_options = 'pinim_options';
 
     var $boards_followed_urls = array();
@@ -116,6 +117,7 @@ class PinIm {
     function setup_actions(){  
         add_action( 'plugins_loaded', array($this, 'upgrade'));//upgrade
         add_filter( 'plugin_action_links_' . $this->basename, array($this, 'plugin_bottom_links')); //bottom links
+        add_action( 'init', array($this, 'register_post_type') );
         add_action( 'admin_init', array(&$this,'load_textdomain'));
         add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );
         add_action( 'add_meta_boxes', array($this, 'pinim_metabox'));
@@ -129,9 +131,8 @@ class PinIm {
         global $wpdb;
 
         $current_version = get_option("_pinterest-importer-db_version");
-
         if ($current_version==$this->db_version) return false;
-
+        
         if(!$current_version){ //not installed
             /*
             require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
@@ -158,6 +159,16 @@ class PinIm {
                 }
                 update_user_meta( get_current_user_id(), 'pinim_boards_settings', $boards_settings);
             }
+            
+            
+            if($current_version <= '207'){ //switch post type to 'pin'
+                
+                $querystr = $wpdb->prepare( "UPDATE $wpdb->posts table_posts LEFT JOIN $wpdb->postmeta table_metas ON table_posts.ID = table_metas.post_id SET table_posts.post_type = REPLACE(table_posts.post_type, %s, %s) WHERE table_metas.meta_key = '%s'", 'post', self::$pin_post_type,'_pinterest-pin_id' );
+                
+                $result = $wpdb->get_results ( $querystr );
+                
+            }
+            
             if($current_version < '206'){
                 $boards_settings = pinim_get_boards_options();
                 foreach((array)$boards_settings as $key=>$board){
@@ -173,6 +184,60 @@ class PinIm {
 
         //update DB version
         update_option("_pinterest-importer-db_version", $this->db_version );
+
+    }
+
+    function register_post_type() {
+
+        $labels = array(
+            'name'                  => _x( 'Pins', 'Post Type General Name', 'pinim' ),
+            'singular_name'         => _x( 'Pin', 'Post Type Singular Name', 'pinim' ),
+            'menu_name'             => __( 'Pins', 'pinim' ),
+            'name_admin_bar'        => __( 'Pin', 'pinim' ),
+            'archives'              => __( 'Item Archives', 'pinim' ),
+            'attributes'            => __( 'Item Attributes', 'pinim' ),
+            'parent_item_colon'     => __( 'Parent Item:', 'pinim' ),
+            'all_items'             => __( 'All Items', 'pinim' ),
+            'add_new_item'          => __( 'Add New Item', 'pinim' ),
+            'add_new'               => __( 'Add New', 'pinim' ),
+            'new_item'              => __( 'New Item', 'pinim' ),
+            'edit_item'             => __( 'Edit Item', 'pinim' ),
+            'update_item'           => __( 'Update Item', 'pinim' ),
+            'view_item'             => __( 'View Item', 'pinim' ),
+            'view_items'            => __( 'View Items', 'pinim' ),
+            'search_items'          => __( 'Search Item', 'pinim' ),
+            'not_found'             => __( 'Not found', 'pinim' ),
+            'not_found_in_trash'    => __( 'Not found in Trash', 'pinim' ),
+            'featured_image'        => __( 'Featured Image', 'pinim' ),
+            'set_featured_image'    => __( 'Set featured image', 'pinim' ),
+            'remove_featured_image' => __( 'Remove featured image', 'pinim' ),
+            'use_featured_image'    => __( 'Use as featured image', 'pinim' ),
+            'insert_into_item'      => __( 'Insert into item', 'pinim' ),
+            'uploaded_to_this_item' => __( 'Uploaded to this item', 'pinim' ),
+            'items_list'            => __( 'Items list', 'pinim' ),
+            'items_list_navigation' => __( 'Items list navigation', 'pinim' ),
+            'filter_items_list'     => __( 'Filter items list', 'pinim' ),
+        );
+        $args = array(
+            'label'                 => __( 'Pin', 'pinim' ),
+            'description'           => __( 'Posts imported from Pinterest', 'pinim' ),
+            'labels'                => $labels,
+            'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'custom-fields', 'post-formats', ),
+            'taxonomies'            => array( 'category', 'post_tag' ),
+            'hierarchical'          => false,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'menu_position'         => 5,
+            'show_in_admin_bar'     => true,
+            'show_in_nav_menus'     => true,
+            'can_export'            => true,
+            'has_archive'           => true,		
+            'exclude_from_search'   => false,
+            'publicly_queryable'    => true,
+            'capability_type'       => 'post',
+        );
+        register_post_type( self::$pin_post_type, $args );
 
     }
     
